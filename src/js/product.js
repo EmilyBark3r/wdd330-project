0aa3ae44b11e6a9923bdcbead664c3334005eb84
BarkerEmily <emilybarker@byui.edu>
2023-05-09 15:00:32 -0600

    commit

--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,16 @@
-import { setLocalStorage, getParam } from "./utils.mjs";
+import { setLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
-import { productDetails } from "./productDetails.mjs";
 
-//
-const productId = getParam('product');
-productDetails(productId, ".product-detail");
-console.log(findProductById(productId));
-                
+function addProductToCart(product) {
+  setLocalStorage("so-cart", product);
+}
+// add to cart button event handler
+async function addToCartHandler(e) {
+  const product = await findProductById(e.target.dataset.id);
+  addProductToCart(product);
+}
+
+// add listener to Add to Cart button
+document
+  .getElementById("addToCart")
+  .addEventListener("click", addToCartHandler);
