062b1a7a09db220c3ef5733b8faeddaf78ac02c2
mabelheiner <83719321+mabelheiner@users.noreply.github.com>
2023-05-11 15:50:51 -0600

    Co-authored-by: Samantha Thueson <SamiThueson@users.noreply.github.com> Co-authored-by: emilyrowley1 <emilyrowley1@users.noreply.github.com> Co-authored-by: EmilyBark3r <EmilyBark3r@users.noreply.github.com>
    Team Activity 3 done! Products Dynamically added

---
+++ src/js/components/ProductList.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { getData } from "../productData.mjs";
+  import ProductSummary from "./ProductSummary.svelte";
+
+  // this is how we make a prop in svelte
+  export let category;
+  // if you are looking at this thinking that's strange to just stop with a promise
+  // you would be right.  This will make more sense in a bit...stay tuned.
+  let promise = getData(category);
+  let blackList = ["989CG", "880RT"];
+</script>
+
+<h2>Top products: {category}</h2>
+{#await promise}
+  Loading
+{:then products}
+  <ul class="product-list">
+    {#each products as product}
+      {#if !blackList.includes(product.Id)}
+        <li class="product-card">
+          <ProductSummary {product} />
+        </li>
+      {/if}
+    {/each}
+  </ul>
+{/await}
